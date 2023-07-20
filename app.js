a9751ec925ae37da76b9122e56647a200f14d6d9
hapill17 <hapill17@gmail.com>
2023-07-20 20:38:12 +0900

    Add list.ejs file

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
-
 const dotenv = require('dotenv');
 dotenv.config();
+app.set('view engine','ejs');
 
-app.get("/write",function (req,res) {
-    res.sendFile(__dirname+"/write.html")
+app.get("/add",function (req,res) {
+    res.sendFile(__dirname+"/add.html")
+});
+
+app.get("/list",function (req,res) {
+  res.render('list.ejs')
 });
 
 var db;
@@ -18,17 +22,15 @@ MongoClient.connect(process.env.DB_URL, function(error, client){
   db = client.db('todoapp');
 
   app.post('/add', function(req, res){
+    res.send('Post data to the page.');
    db.collection('post').insertOne( {date: req.body.date  , title : req.body.title} , function(error, result){
-     console.log('save the data in db.')
-     app.listen(process.env.PORT, function(){
-     console.log('listening on 3000')
-     });
+     console.log('Save the data in db.')
    });
-   app.listen(process.env.PORT, function(){
-    console.log('listening on 3000')
    })
-  })
-});
+  });
 
+  app.listen(process.env.PORT, function(){
+    console.log('listening on 3000')
+    });
 
-
+    
